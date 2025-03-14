7d8792cf1c7f009a562c76a793cc75024a48c05c
joshuaplischek <plischekjoshua@web.de>
2025-03-14 11:29:21 +0100

    Added and Design Container

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 
 function init(){
+    renderBooks()
+}
 
-   for (let i = 0; i < books.length; i++) {
-        console.table(books[i])
+function renderBooks() {
+    let sliderRef = document.getElementById("book_slider");
+    sliderRef.innerHTML0="";
+
+    for (let i = 0; i < books.length; i++) {
+        sliderRef.innerHTML += getBookTemplate(i);
     
     }
+}
 
+function getBookTemplate(i) {
+    return/*html*/`
+        <div class="bookContainer">
+            <div class="titleArea"><p>Titel</p></div>
+            <div><img src="" alt=""></div>
+            <div></div>
+            <div>
+                <input type="text">
+                <button onclick=""></button>
+            </div>
+            
+
+        </div>
+    `
 }
