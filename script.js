b97803dda8429a0fa08d93706c3e19d192958fee
joshuaplischek <plischekjoshua@web.de>
2025-03-15 22:33:33 +0100

    add some js

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
+let bookIsLiked = "./img/fav.png";
+let bookIsNotLikesd = "./img/fav -liked.png";
 
 function init(){
-    renderBooks()
+    renderBooks();
 }
 
 function renderBooks() {
-    let sliderRef = document.getElementById("content");
-    sliderRef.innerHTML0="";
+    let contentRef = document.getElementById("content");
+    contentRef.innerHTML0="";
 
     for (let i = 0; i < books.length; i++) {
-        sliderRef.innerHTML += getBookTemplate(i);
+        contentRef.innerHTML += getBookTemplate(i);
+    }
+    checkLikedBooks()
+}
+
+function likeBook(){
     
+}
+
+function checkLikedBooks(i) {
+    let likeOrUnlike = document.getElementById("toggelLikes").innerHTML;
+    let likedBook = Object.key(books[i].liked);
+    if (likedBook = true) {
+        likebookTrueOrFlase = likeStatus[0]
     }
 }
+
+
 
 function getBookTemplate(i) {
     return/*html*/`
         <div class="bookContainer">
-            <div class="titleArea"><p>Titel</p></div>
+            <div class="titleArea"><p>${books[i].name}</p></div>
             <div class="line"></div>
             <div class="image-div"><img src="./img/book_cover.png" alt=""></div>
             <div class="line"></div>
-            <div class="informations"></div>
+            <div class="informations">
+                <div class="price-like-container">
+                    <div id="bookPrice"><p>${books[i].price} €</p></div>
+                    <div id="likeButton">
+                        <p class="like-counter">${books[i].likes}</p> 
+                        <img src="./img/fav.png" alt="">
+                    </div>
+                </div>
+                <table>
+                    <tr>
+                        <td>Author</td>
+                        <td>:${books[i].author}</td>
+                    </tr>
+                    <tr>
+                        <td>Erscheinungsjahr</td>
+                        <td>:${books[i].publishedYear}</td>
+                    </tr>
+                    <tr>
+                        <td>Genre</td>
+                        <td>:${books[i].genre}</td>
+                    </tr>
+                </table>
+            </div>
             <div class="line"></div>
             <div class="comments"></div>
             <div class="write-comments">
