96ad205fc8b72b02f6a857f28b271b54c0e3d636
joshuaplischek <plischekjoshua@web.de>
2025-03-19 09:18:50 +0100

    Project maybe finished

--- scripts/books.js
+++ scripts/books.js
@@ -1,175 +1,175 @@
 let books = [
-    {
-      "name": "Jeden Tag einen Schritt",
-      "author": "Hoss & Hopf",
-      "likes": 1250,
-      "liked": true,
-      "price": 19.99,
-      "publishedYear": 2018,
-      "genre": "Fantasy",
-      "comments": [
-        {
-          "username": "Leser123",
-          "comment": "Für jeden Menschen und jede Altersgruppe geeignet - neue Ideen - interessante Impulse."
-        },
-        {
-          "username": "Bookworm84",
-          "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
-        },
-        {
-          "username": "FantasyFanatic",
-          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
-        },
-        {
-          "username": "SciFiGuru",
-          "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
-        },
-        {
-          "username": "NovelLover",
-          "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
-        }
-      ]
-    },
-    {
-      "name": "Der vergessene Pfad",
-      "author": "Maximilian Schwarz",
-      "likes": 980,
-      "liked": false,
-      "price": 14.49,
-      "publishedYear": 2021,
-      "genre": "Fantasy",
-      "comments": []
-    },
-    {
-      "name": "Die Farben des Himmels",
-      "author": "Laura Blau",
-      "likes": 1520,
-      "liked": true,
-      "price": 22.95,
-      "publishedYear": 2019,
-      "genre": "Romantik",
-      "comments": [
-        {
-          "username": "LeserPeter",
-          "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
-        },
-        {
-          "username": "BookLover21",
-          "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
-        },
-        {
-          "username": "FantasyNerd",
-          "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
-        },
-        {
-          "username": "SciFiEnthusiast",
-          "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
-        },
-        {
-          "username": "ReadingAddict",
-          "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das Rätsel der Zeit",
-      "author": "Alexander Weiss",
-      "likes": 750,
-      "liked": false,
-      "price": 18.99,
-      "publishedYear": 2020,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "username": "BuchKenner",
-          "comment": "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat."
-        },
-        {
-          "username": "LeseWurm",
-          "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
-        }
-      ]
-    },
-    {
-      "name": "Der letzte Wächter",
-      "author": "Sabine Grün",
-      "likes": 1300,
-      "liked": true,
-      "price": 16.75,
-      "publishedYear": 2017,
-      "genre": "Fantasy",
-      "comments": []
-    },
-    {
-      "name": "Im Schatten des Mondes",
-      "author": "Philipp Silber",
-      "likes": 890,
-      "liked": false,
-      "price": 12.49,
-      "publishedYear": 2022,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "username": "BücherLiebhaber",
-          "comment": "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd."
-        },
-        {
-          "username": "Leseratte",
-          "comment": "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat."
-        }
-      ]
-    },
-    {
-      "name": "Jenseits der Sterne",
-      "author": "Oliver Schwarz",
-      "likes": 1450,
-      "liked": true,
-      "price": 21.99,
-      "publishedYear": 2015,
-      "genre": "Science-Fiction",
-      "comments": [
-        {
-          "username": "Leser123",
-          "comment": "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat."
-        }
-      ]
-    },
-    {
-      "name": "Das verborgene Königreich",
-      "author": "Elena Gold",
-      "likes": 920,
-      "liked": false,
-      "price": 17.99,
-      "publishedYear": 2020,
-      "genre": "Fantasy",
-      "comments": [
-        {
-          "username": "Bookworm92",
-          "comment": "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat."
-        }
-      ]
-    },
-    {
-      "name": "Liebe in Zeiten des Krieges",
-      "author": "Emilia Rot",
-      "likes": 1800,
-      "liked": true,
-      "price": 19.99,
-      "publishedYear": 2016,
-      "genre": "Romantik",
-      "comments": [
-        {
-          "username": "Bibliophile23",
-          "comment": "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen."
-        },
-        {
-          "username": "StorySeeker",
-          "comment": "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat."
-        },
-        {
-          "username": "SciFiExplorer",
-          "comment": "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig."
-        }
-      ]
-    }
-  ]
+  {
+    "name": "Jeden Tag einen Schritt",
+    "author": "Hoss & Hopf",
+    "likes": 1250,
+    "liked": true,
+    "price": 19.99,
+    "publishedYear": 2018,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": [
+      {
+        "username": "Leser123",
+        "comment": "Eine inspirierende Lektüre, die mir geholfen hat, meine täglichen Gewohnheiten zu verbessern."
+      },
+      {
+        "username": "Bookworm84",
+        "comment": "Tolle Tipps für persönliche Weiterentwicklung – verständlich und praxisnah."
+      },
+      {
+        "username": "FantasyFanatic",
+        "comment": "Ein Buch, das mich motiviert hat, meine Ziele Schritt für Schritt anzugehen."
+      },
+      {
+        "username": "SciFiGuru",
+        "comment": "Super verständlich und mit vielen realen Beispielen! Sehr hilfreich für den Alltag."
+      },
+      {
+        "username": "NovelLover",
+        "comment": "Jeden Tag eine kleine Verbesserung – genau das, was ich gebraucht habe."
+      }
+    ]
+  },
+  {
+    "name": "Denke nach und werde reich",
+    "author": "Napoleon Hill",
+    "likes": 980,
+    "liked": false,
+    "price": 14.99,
+    "publishedYear": 1937,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": []
+  },
+  {
+    "name": "Die 7 Wege zur Effektivität",
+    "author": "Stephen R. Covey",
+    "likes": 1520,
+    "liked": true,
+    "price": 22.95,
+    "publishedYear": 1989,
+    "genre": "Selbstmanagement",
+    "comments": [
+      {
+        "username": "LeserPeter",
+        "comment": "Ein Buch, das mein Denken und Handeln nachhaltig verändert hat."
+      },
+      {
+        "username": "BookLover21",
+        "comment": "Sehr tiefgründig und mit vielen praktischen Ansätzen – absolut empfehlenswert!"
+      },
+      {
+        "username": "FantasyNerd",
+        "comment": "Strukturierte Methoden, um effektiver zu werden – ich habe viel daraus gelernt."
+      },
+      {
+        "username": "SciFiEnthusiast",
+        "comment": "Wer sich persönlich weiterentwickeln will, sollte dieses Buch lesen!"
+      },
+      {
+        "username": "ReadingAddict",
+        "comment": "Einer der besten Ratgeber für Produktivität und Selbstmanagement."
+      }
+    ]
+  },
+  {
+    "name": "Die Gesetze der Gewinner",
+    "author": "Bodo Schäfer",
+    "likes": 750,
+    "liked": false,
+    "price": 18.99,
+    "publishedYear": 2001,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": [
+      {
+        "username": "BuchKenner",
+        "comment": "Klare Prinzipien für Erfolg – einfach umzusetzen und sehr motivierend."
+      },
+      {
+        "username": "LeseWurm",
+        "comment": "Sehr praxisnah und direkt anwendbar, top Buch!"
+      }
+    ]
+  },
+  {
+    "name": "Rich Dad Poor Dad",
+    "author": "Robert T. Kiyosaki",
+    "likes": 1300,
+    "liked": true,
+    "price": 14.75,
+    "publishedYear": 1997,
+    "genre": "Mindset",
+    "comments": []
+  },
+  {
+    "name": "Atomic Habits Die 1% Methode",
+    "author": "James Clear",
+    "likes": 890,
+    "liked": false,
+    "price": 22.49,
+    "publishedYear": 2018,
+    "genre": "Persönlichkeitsentwicklung",
+    "comments": [
+      {
+        "username": "BücherLiebhaber",
+        "comment": "Jede kleine Gewohnheit zählt! Dieses Buch hat mir sehr geholfen."
+      },
+      {
+        "username": "Leseratte",
+        "comment": "Tolle wissenschaftlich fundierte Ansätze zur Verbesserung der eigenen Routinen."
+      }
+    ]
+  },
+  {
+    "name": "The One Thing",
+    "author": "Gary Keller & Jay Papasan",
+    "likes": 1450,
+    "liked": true,
+    "price": 19.99,
+    "publishedYear": 2013,
+    "genre": "Produktivität",
+    "comments": [
+      {
+        "username": "Leser123",
+        "comment": "Hilft enorm, sich auf das Wesentliche zu konzentrieren – großartige Ansätze!"
+      }
+    ]
+  },
+  {
+    "name": "Die 10X-Regel",
+    "author": "Grant Cardone",
+    "likes": 920,
+    "liked": false,
+    "price": 17.99,
+    "publishedYear": 2011,
+    "genre": "Motivation",
+    "comments": [
+      {
+        "username": "Bookworm92",
+        "comment": "Ein echtes Power-Buch – zeigt, dass man seine Ziele viel größer setzen sollte!"
+      }
+    ]
+  },
+  {
+    "name": "Das Robbins Power Prinzip",
+    "author": "Tony Robbins",
+    "likes": 1800,
+    "liked": true,
+    "price": 29.99,
+    "publishedYear": 1991,
+    "genre": "Motivation",
+    "comments": [
+      {
+        "username": "Bibliophile23",
+        "comment": "Ein Klassiker der Motivation – absolut mitreißend!"
+      },
+      {
+        "username": "StorySeeker",
+        "comment": "Tony Robbins zeigt, wie man sein Leben selbst in die Hand nimmt."
+      },
+      {
+        "username": "SciFiExplorer",
+        "comment": "Dieses Buch gibt einem den mentalen Push, um wirklich durchzustarten."
+      }
+    ]
+  }
+]
